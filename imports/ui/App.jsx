575bdb7d6301a018a37f03270ee106efdc41749c
Josh <Afro523>
2017-02-20 20:29:48 -0500

    few things

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import {Link} from 'react-router';
 //MUi stuff
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
