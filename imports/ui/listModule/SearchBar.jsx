609df52d6348a04b461594861f30cee2d5520b1c
Afro523 <Jresnick523@gmail.com>
2017-06-12 22:27:25 -0400

    removed materialize css stylesheet import
    changed from url import to module import

--- imports/ui/listModule/SearchBar.jsx
+++ imports/ui/listModule/SearchBar.jsx
@@ -4,6 +4,7 @@ import React, {Component} from 'react';
 //Material UI
 import TextField from 'material-ui/TextField';
 import FontIcon from 'material-ui/FontIcon';
+import {grey400} from 'material-ui/styles/colors';
 
 export default class SearchBar extends Component {
 	constructor(props) {
@@ -19,15 +20,21 @@ export default class SearchBar extends Component {
 			<div style={{height:'48px', position:'relative'}}>
 				<div
 					style={{
+						border:'1px grey solid',
+						borderRadius:'0px 0px 10px 10px',
 						width:'100%',
 						position:'fixed',
 						display:'flex',
 						alignItems:'center',
 						zIndex:5,
-						backgroundColor:'white'}}>
+						backgroundColor:grey400}}>
 					<FontIcon style={{fontSize:'32px'}} className="material-icons">search</FontIcon>
 					<TextField
+						underlineFocusStyle={{border:'0px'}}
 						hintText="Search"
+						fullWidth={true}
+						style={{marginBottom:'0px'}}
+						underlineStyle={{display: 'none'}}
 					/>
 				</div>
 			</div>
